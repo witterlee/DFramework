636db076d7f34041f8159313e79d87b3168d16c5
weitaolee <12666954@qq.com>
2015-08-20 23:12:52 +0800

    clear code

--- DFramework - 副本 (2)/Command/AbstractCommandExecutor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace DFramework
-{
-    public abstract class AbstractCommandExecutor<TCommand> : ICommandExecutor<TCommand> where TCommand : ICommand
-    {
-        public abstract Task ExecuteAsync(TCommand cmd); 
-    }
-}
