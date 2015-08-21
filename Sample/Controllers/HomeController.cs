e99cfb3c3453ceaf5493c2926129c79f947ae9b3
weitaolee <12666954@qq.com>
2015-08-21 09:00:11 +0800

    add test

--- Sample/Controllers/HomeController.cs
+++ Sample/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Sample.Command;
@@ -16,5 +18,29 @@ namespace Sample.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<ActionResult> IndexLoop()
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            int i = 1000, j = 10;
+
+            var taskList = new List<Task>();
+
+            while (j-- > 0)
+            {
+                i = 10000;
+                while (i-- > 0)
+                {
+                    var cmd = new TestCommand("nameA", "pwdB");
+                    taskList.Add(CommandBus.SendAsync(cmd));
+                }
+
+                await Task.WhenAll(taskList);
+            }
+            sw.Stop();
+            ViewBag.Time = sw.ElapsedMilliseconds / 1000;
+            return View();
+        }
     }
 }
